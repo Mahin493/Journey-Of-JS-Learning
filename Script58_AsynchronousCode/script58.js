f4ac9b94264dafe413c56e85849f60c9ccc6ec37
MahinOpu <opuaust17@gmail.com>
2022-01-10 11:52:54 +0600

    ScriptJs Asynchronous finished

---
+++ Script58_AsynchronousCode/script58.js
@@ -0,0 +1,27 @@
+setTimeout(()=>{
+    console.log("inside")
+    setTimeout(()=>{
+        console.log("Inside 2")
+    },100)
+  //nesting call back inside callback make Callback hell
+    setTimeout(()=>{
+        console.log("Inside 3")
+    },1000)
+},1000)
+
+
+//Asynchronous code is something that does not execute in a normal way.
+const buttonb=document.querySelector(".mybutton")
+//buttonb.addEventListener('click',() =>{
+    addClickEventListener(buttonb,()=>{
+    console.log('Clicked')
+
+})
+
+
+
+function addClickEventListener(element,callback){
+    element.addEventListener("click",callback)
+}
+
+console.log("Outside")

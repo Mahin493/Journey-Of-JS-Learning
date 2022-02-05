ddfd6f643c10ead5c2ca04fcefc75f61c94e0d7d
Mahiat Mahin <42869069+Mahin493@users.noreply.github.com>
2022-02-05 16:52:01 +0600

    Promises Async Await

---
+++ Script60_Async Await/script60.js
@@ -0,0 +1,93 @@
+function setTimeoutPromise(delay){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            //reject('Error')
+            resolve(`you waited ${delay} milliseconds`)
+            
+         },delay)
+    })
+}
+
+/*
+setTimeoutPromise(650).then((message)=>{
+    console.log(message)
+    console.log("1")
+    return setTimeoutPromise(250)
+})
+.then(()=>{
+    console.log("2")
+})
+*/
+// Do this by Async way
+
+async function doStuff(){
+   try{
+     const message=await setTimeoutPromise(250)
+    
+    console.log(message)
+    console.log("1")
+    const message2=await setTimeoutPromise(250)
+    console.log(message2)
+    console.log("5")
+   }
+   catch(error){
+     console.log(error)
+   }
+
+    
+}
+
+doStuff()
+
+//My Task:-
+
+function getValueWithDelay(value,delay){
+    return new Promise((resolve,rejecy)=>{
+
+        setTimeout(()=>{
+          resolve(value)
+        },delay)
+       
+    })
+}
+
+
+
+function getValueWithdelayError(value,delay){
+    return new Promise((resolve,reject)=>{
+
+        setTimeout(()=>{
+          reject('Error')
+        },delay)
+       
+    })
+}
+
+// Call getValuewithDelay twice and print out the returned value
+//then call getValuewithDelayErroe and make sure that the error is properly
+//caught 
+async function dostuff1(){
+
+    try{
+            const value1=  await getValueWithDelay('Mahin',250)
+             console.log(value1)
+             const value2=  await getValueWithDelay('Apu',250)
+             console.log(value2)
+             
+             const value3=  await getValueWithdelayError('HI',250)
+             console.log(value3)
+    }       
+    catch(e){
+        console.error(e)
+    } finally{
+        console.log("Finally")
+    }
+    
+   
+}
+
+dostuff1()
+
+//shorly can be written:-
+
+getValueWithDelay('Mahiat',250).then(message=>console.log(message))

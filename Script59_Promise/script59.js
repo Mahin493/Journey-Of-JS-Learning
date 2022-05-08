0f327a18c3ab6ada1d5bb74eb76db2e2c9a2cf5c
Mahiat Mahin <42869069+Mahin493@users.noreply.github.com>
2022-05-08 23:12:50 +0600

    Promises

--- Script59_Promise/script59.js
+++ Script59_Promise/script59.js
@@ -1,32 +1,42 @@
-const promise=new Promise((resolve,reject)=>{
-const multiply=5*3
+// const promise=new Promise((resolve,reject)=>{
+// const multiply=5*3
 
-if(multiply==20){
-    resolve("Success")
-}
-else{
-    reject("Error")
-}
+// if(multiply==20){
+//     resolve("Success")
+// }
+// else{
+//     reject("Error")
+// }
 
-})
+// })
 
-promise.then(message=>{
-    console.log(message)
-})
-.catch(message=>{
-    console.log(message)
-})
+// promise.then(message=>{
+//     console.log(message)
+// })
+// .catch(message=>{
+//     console.log(message)
+// })
 
 //Asynchronous SettimeOut vs Promise
 
-setTimeout(()=>{
-    console.log("Here")
-},250)
+// setTimeout(()=>{
+//     console.log("Here")
+// },250)
 
-function setTimeoutPromise(){
-  return  new promise((resolve,reject)=>{
-        setTimeout(() => {
-            console.log("here")
-        }, 250);
+setTimeoutPromise(250).then(()=>{
+      console.log("then")
+})
+
+function setTimeoutPromise(duration){
+  return  new Promise((resolve,reject)=>{
+        setTimeout(resolve,duration)
     })
+}
+
+//A promise version of addEventListener
+const butoon=document.querySelector('button')
+function addEventListenerPromise(element,method){
+      return new Promise((resolve,reject) =>{
+            element.addEventListeer(method,resove)
+      })
 }
